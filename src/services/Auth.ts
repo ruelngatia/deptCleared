39cc824a8245ccd6f25caec32c21a0d7b2c983ab
ruelngatia <ruelngatia@gmail.com>
2024-03-11 18:02:04 +0300

    new server

--- src/services/Auth.ts
+++ src/services/Auth.ts
@@ -4,7 +4,7 @@ import { LoginModel } from "../models/LoginModel";
 
 export class Auth {
   // public baseURL: string = "http://localhost:4004";
-  public baseURL: string = "https://aipredictivecareserver-c4wa.onrender.com";
+  public baseURL: string = "https://deptclearedserver.onrender.com";
 
   public async signin(form: any): Promise<any> {
     try {
